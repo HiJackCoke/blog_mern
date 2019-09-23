a200465bf8b370d44c7e8ce55e753b22bbb02b2c
taehunlim <dkslsdkdl@naver.com>
2019-09-23 20:14:15 +0900

    add bodyParser

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 
 const db = "mongodb+srv://blog:asd123@cluster0-rsbsu.mongodb.net/test?retryWrites=true&w=majority";
 
@@ -13,7 +16,9 @@ mongoose
     .then(()=> console.log("mongoDB connected"))
     .catch(err => console.log(err));
 
-
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
 
 
 const port = 3000;
