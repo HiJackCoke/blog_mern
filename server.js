40505f15d5fb986f2257d0fbb290c0676b339bbb
taehunlim <dkslsdkdl@naver.com>
2019-09-23 20:10:29 +0900

    connect mongoose

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const db = "mongodb+srv://blog:asd123@cluster0-rsbsu.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose
+    .connect(db, {
+        useNewUrlParser : true,
+        useCreateIndex : true,
+        useUnifiedTopology : true
+    })
+    .then(()=> console.log("mongoDB connected"))
+    .catch(err => console.log(err));
+
+
+
 
 const port = 3000;
 
