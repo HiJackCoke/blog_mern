386c29cd5bb6b6fabe8f09823fef258ef59cfbd7
taehunlim <dkslsdkdl@naver.com>
2019-10-12 11:10:19 +0900

    add post model

---
+++ models/post.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+
+    user : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'users'
+    },
+    text : {
+        type : String,
+        required : true
+    },
+    name : {
+        type : String,
+    },
+    avatar : {
+        type : String
+    },
+    likes : [
+        {
+            user : {
+                type : mongoose.Schema.Types.ObjectId,
+                ref : 'users'
+            }
+        }
+    ],
+    comments : [
+        {
+            user : {
+                type : mongoose.Schema.Types.ObjectId,
+                required : true
+            },
+            text : {
+                type : String,
+                required : true
+            },
+            name : {
+                type : String
+            },
+            avatar : {
+                type : String
+            },
+            data : {
+                type : Data,
+                default : Data.now
+            }
+        }
+    ],
+    date : {
+        type : Date,
+        default : Date.now
+    }
+
+    
+
+});
+
+module.exports = mongoose.model('post', postSchema);
